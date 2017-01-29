18e37708c2db9db12fc86ed496f7081af1a50e7f
mtg2 <mtg2@admins-mbp.lan>
2017-01-29 21:02:29 +0000

    Test implemented that creates a test GameObject and running an iteration on it.

--- fungine/src/lib.rs
+++ fungine/src/lib.rs
@@ -1,21 +1,59 @@
+
+#[macro_use]
+extern crate downcast_rs;
+
 #[cfg(test)]
 mod tests {
+	use std::sync::Arc;
+	use fungine::{ Fungine, GameObject, Message };
+
+	#[derive(Clone)]
+	struct TestGameObject {
+		value: i32,
+	}
+
+	impl GameObject for TestGameObject {
+	    fn box_clone(&self) -> Box<GameObject> {
+	        Box::new((*self).clone())
+	    }
+
+	    fn update(self: Box<Self>, current_state: &Vec<Box<GameObject>>, messages: Vec<Message>) -> Box<GameObject> {
+	    	Box::new(TestGameObject {
+	    		value: self.value + 1
+	    	})
+	    }
+	}
+
     #[test]
-    fn it_works() {
+    fn test_iterate() {
+    	let initial_object = TestGameObject {
+    		value: 0
+    	};
+    	let initial_object = Box::new(initial_object) as Box<GameObject>;
+    	let initial_state = Arc::new(vec![initial_object]);
+    	let next_state = Fungine::step_engine(initial_state);
+    	if let Some(next_object) = next_state[0].downcast_ref::<TestGameObject>() {
+    		assert!(next_object.value == 1);
+    	} 
+    	else {
+    		assert!(false);
+    	}
     }
 }
 
 mod fungine {
 	use std::sync::Arc;
+	use downcast_rs::Downcast;
 
-	struct Message {
+	pub struct Message {
 
 	}
 
-	trait GameObject {
+	pub trait GameObject: Downcast {
 		fn update(self: Box<Self>, current_state: &Vec<Box<GameObject>>, messages: Vec<Message>) -> Box<GameObject>;
 		fn box_clone(&self) -> Box<GameObject>;
 	}
+	impl_downcast!(GameObject);
 
 	impl Clone for Box<GameObject>
 	{
@@ -24,14 +62,20 @@ mod fungine {
 	    }
 	}
 
-	struct Fungine {
+	pub struct Fungine {
 		initial_state: Vec<Box<GameObject>>,
 	}
 
 	impl Fungine {
-		fn run(self) {
+		pub fn run(self) {
+			let mut states: Arc<Vec<Box<GameObject>>> = Arc::new(self.initial_state);
+			loop {
+				states = Fungine::step_engine(states);
+			}
+		}
+
+		pub fn step_engine(states: Arc<Vec<Box<GameObject>>>) -> Arc<Vec<Box<GameObject>>> {
 			let mut next_states: Vec<Box<GameObject>> = Vec::new();
-			let states: Arc<Vec<Box<GameObject>>> = Arc::new(Vec::new());
 			for x in 0..states.len() {
 				let states = states.clone();
 				let messages: Vec<Message> = Vec::new();
@@ -39,6 +83,7 @@ mod fungine {
 				let next_state = state.update(&states, messages);
 				next_states.push(next_state);
 			}
+			Arc::new(next_states)
 		}
 	}
 }
