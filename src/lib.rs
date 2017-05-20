301d44fbfdafe3d1cc3752ad85a97e0b50f3aa45
Mike Goddard <mtg2@CCs-MacBook-Pro.local>
2017-05-20 20:49:35 +0100

    Brought public interface into line with the multi-threaded branch.

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ mod fungine {
 	}
 
 	pub trait GameObject: Downcast {
-		fn update(self: Box<Self>, current_state: &Vec<Box<GameObject>>, messages: Vec<Message>) -> Box<GameObject>;
+		fn update(&self, current_state: Arc<Vec<Arc<Box<GameObject>>>>, messages: Vec<Message>) -> Box<GameObject>;
 		fn box_clone(&self) -> Box<GameObject>;
 	}
 	impl_downcast!(GameObject);
@@ -24,25 +24,39 @@ mod fungine {
 	}
 
 	pub struct Fungine {
-		initial_state: Vec<Box<GameObject>>,
+		initial_state: Arc<Vec<Arc<Box<GameObject>>>>,
 	}
 
 	impl Fungine {
+		pub fn new(initial_state: Arc<Vec<Arc<Box<GameObject>>>>) -> Fungine {
+			Fungine {
+				initial_state: initial_state
+			}
+		}
+
 		pub fn run(self) {
-			let mut states: Arc<Vec<Box<GameObject>>> = Arc::new(self.initial_state);
+			let mut states: Arc<Vec<Arc<Box<GameObject>>>> = self.initial_state;
 			loop {
 				states = Fungine::step_engine(states);
 			}
 		}
 
-		pub fn step_engine(states: Arc<Vec<Box<GameObject>>>) -> Arc<Vec<Box<GameObject>>> {
-			let mut next_states: Vec<Box<GameObject>> = Vec::new();
+		pub fn run_steps(self, steps: u32) -> Arc<Vec<Arc<Box<GameObject>>>> {
+			let mut states: Arc<Vec<Arc<Box<GameObject>>>> = self.initial_state;
+			for _ in 0..steps {
+				states = Fungine::step_engine(states);
+			}
+			states
+		}
+
+		fn step_engine(states: Arc<Vec<Arc<Box<GameObject>>>>) -> Arc<Vec<Arc<Box<GameObject>>>> {
+			let mut next_states: Vec<Arc<Box<GameObject>>> = Vec::new();
 			for x in 0..states.len() {
 				let states = states.clone();
-				let messages: Vec<Message> = Vec::new();
 				let state = states[x].clone();
-				let next_state = state.update(&states, messages);
-				next_states.push(next_state);
+				let messages: Vec<Message> = Vec::new();
+				let next_state = state.update(states, messages);
+				next_states.push(Arc::new(next_state));
 			}
 			Arc::new(next_states)
 		}
@@ -65,7 +79,7 @@ mod tests {
 	        Box::new((*self).clone())
 	    }
 
-	    fn update(self: Box<Self>, current_state: &Vec<Box<GameObject>>, messages: Vec<Message>) -> Box<GameObject> {
+	    fn update(&self, current_state: Arc<Vec<Arc<Box<GameObject>>>>, messages: Vec<Message>) -> Box<GameObject> {
 	    	Box::new(TestGameObject {
 	    		value: self.value + 1
 	    	})
@@ -78,8 +92,9 @@ mod tests {
     		value: 0
     	};
     	let initial_object = Box::new(initial_object) as Box<GameObject>;
-    	let initial_state = Arc::new(vec![initial_object]);
-    	let next_state = Fungine::step_engine(initial_state);
+    	let initial_state = Arc::new(vec![Arc::new(initial_object)]);
+		let engine = Fungine::new(initial_state);
+    	let next_state = engine.run_steps(1);
     	if let Some(next_object) = next_state[0].downcast_ref::<TestGameObject>() {
     		assert!(next_object.value == 1);
     	}
@@ -96,16 +111,16 @@ mod tests {
     			value: 0
     		};
     		let initial_object = Box::new(initial_object) as Box<GameObject>;
-    		initial_state.push(initial_object);
+    		initial_state.push(Arc::new(initial_object));
     	}
+		let initial_state = Arc::new(initial_state);
     	let sw = Stopwatch::start_new();
-    	let mut current_state = Fungine::step_engine(Arc::new(initial_state));
-    	for _ in 0..1000 {
-    		current_state = Fungine::step_engine(current_state);
-    	}
-    	for x in 0..current_state.len() {
-    		if let Some(object) = current_state[x].downcast_ref::<TestGameObject>() {
-    			assert!(object.value == 1001);
+		let engine = Fungine::new(initial_state);
+    	let final_states = engine.run_steps(1000);
+    	for x in 0..final_states.len() {
+			let state = final_states[x].clone();
+    		if let Some(state) = state.downcast_ref::<TestGameObject>() {
+    			assert!(state.value == 1000);
 	    	}
 	    	else {
 	    		assert!(false);
