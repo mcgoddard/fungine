f316de37354c7c82e8d69bb53d3c2615a1ba81bd
Michael Goddard <mikeygiom@gmail.com>
2018-01-08 01:43:08 +0000

    Messaging hooked up and tested.

--- src/lib.rs
+++ src/lib.rs
@@ -30,7 +30,7 @@ pub mod fungine {
     // The GameObject trait will be implemented by everything that wants to be executed
     // as part of a frame step by the engine.
     pub trait GameObject: Downcast + Send + Sync {
-        fn update(&self, current_state: Arc<Vec<GameObjectWithID>>, messages: Arc<Vec<Box<Message>>>, time: f32) -> UpdateResult;
+        fn update(&self, current_state: Arc<Vec<GameObjectWithID>>, messages: Arc<Vec<Arc<Box<Message>>>>, time: f32) -> UpdateResult;
         fn box_clone(&self) -> Box<GameObject>;
     }
     impl_downcast!(GameObject);
@@ -43,7 +43,7 @@ pub mod fungine {
     }
 
     #[derive(Clone)]
-    pub struct GameObjectWithState(GameObjectWithID, Arc<Vec<GameObjectWithID>>, f32, Arc<Vec<Box<Message>>>);
+    pub struct GameObjectWithState(GameObjectWithID, Arc<Vec<GameObjectWithID>>, f32, Arc<Vec<Arc<Box<Message>>>>);
 
     #[derive(Clone)]
     pub struct GameObjectWithID {
@@ -59,8 +59,8 @@ pub mod fungine {
 
     #[derive(Clone)]
     pub struct UpdateResult {
-        state: Box<GameObject>,
-        messages: Vec<Box<MessageWithID>>
+        pub state: Box<GameObject>,
+        pub messages: Vec<Arc<Box<MessageWithID>>>
     }
 
     #[derive(Clone)]
@@ -75,7 +75,7 @@ pub mod fungine {
         sends: Vec<Sender<GameObjectWithState>>,
         receiver: Receiver<UpdateResultWithID>,
         pub current_state: Arc<Vec<GameObjectWithID>>,
-        messages: HashMap<u64, Arc<Vec<Box<Message>>>>
+        pub messages: Arc<HashMap<u64, Arc<Vec<Arc<Box<Message>>>>>>
     }
 
     impl Fungine {
@@ -107,7 +107,7 @@ pub mod fungine {
                                 let original: GameObjectWithState = original;
                                 let current_object: Arc<Box<GameObject>> = Arc::clone(&(original.0).game_object);
                                 let current_state: Arc<Vec<GameObjectWithID>> = Arc::clone(&original.1);
-                                let messages: Arc<Vec<Box<Message>>> = Arc::new(Vec::new());
+                                let messages: Arc<Vec<Arc<Box<Message>>>> = original.3;
                                 let new = current_object.update(current_state, messages, original.2);
                                 send_modified.send(UpdateResultWithID {
                                     id: (original.0).id,
@@ -129,7 +129,7 @@ pub mod fungine {
                 sends: sends,
                 receiver: receiver,
                 current_state: Arc::clone(initial_state),
-                messages: HashMap::new()
+                messages: Arc::new(HashMap::new())
             }
         }
 
@@ -138,6 +138,7 @@ pub mod fungine {
             let mut states: Arc<Vec<GameObjectWithID>> = Arc::clone(&self.initial_state);
             let mut sw = Stopwatch::start_new();
             let mut frame_count = 0;
+            let mut messages = Arc::clone(&self.messages);
 
             loop {
                 if sw.elapsed_ms() > 10_000 {
@@ -145,7 +146,9 @@ pub mod fungine {
                     sw.restart();
                     frame_count = 0;
                 }
-                states = Fungine::step_engine(&states, &self.sends, &self.receiver, (sw.elapsed_ms()/1000) as f32, &self.messages);
+                let result = Fungine::step_engine(&states, &self.sends, &self.receiver, (sw.elapsed_ms()/1000) as f32, &messages);
+                states = result.0;
+                messages = result.1;
                 frame_count += 1;
                 sw.restart();
             }
@@ -154,9 +157,12 @@ pub mod fungine {
         // Step the engine forward a specified number of steps, used for testing.
         pub fn run_steps(&self, steps: u32, time_between: f32) -> Arc<Vec<GameObjectWithID>> {
             let mut states: Arc<Vec<GameObjectWithID>> = Arc::clone(&self.initial_state);
+            let mut messages = Arc::clone(&self.messages);
 
             for _ in 0..steps {
-                states = Fungine::step_engine(&states, &self.sends, &self.receiver, time_between, &self.messages);
+                let result = Fungine::step_engine(&states, &self.sends, &self.receiver, time_between, &messages);
+                states = Arc::clone(&result.0);
+                messages = Arc::clone(&result.1);
             }
 
             states
@@ -165,25 +171,36 @@ pub mod fungine {
         // Step the engine forward a specified number of steps from a provided state.
         pub fn run_steps_cont(&mut self, steps: u32, time_between: f32) -> Arc<Vec<GameObjectWithID>> {
             let mut states: Arc<Vec<GameObjectWithID>> = Arc::clone(&self.current_state);
+            let mut messages = Arc::clone(&self.messages);
 
             for _ in 0..steps {
-                states = Fungine::step_engine(&states, &self.sends, &self.receiver, time_between, &self.messages);
+                let result = Fungine::step_engine(&states, &self.sends, &self.receiver, time_between, &messages);
+                states = Arc::clone(&result.0);
+                messages = result.1;
             }
 
             self.current_state = Arc::clone(&states);
+            self.messages = messages;
 
             states
         }
 
         // Perform one step by processing each GameObject in the state once.
         fn step_engine(states: &Arc<Vec<GameObjectWithID>>, sends: &[Sender<GameObjectWithState>], 
-            receiver: &Receiver<UpdateResultWithID>, time: f32, messages: &HashMap<u64, Arc<Vec<Box<Message>>>>) -> Arc<Vec<GameObjectWithID>> {
+            receiver: &Receiver<UpdateResultWithID>, time: f32, messages: &Arc<HashMap<u64, Arc<Vec<Arc<Box<Message>>>>>>)
+            -> (Arc<Vec<GameObjectWithID>>, Arc<HashMap<u64, Arc<Vec<Arc<Box<Message>>>>>>) {
             // Send current states to the worker threads
             for i in 0..states.len() {
                 let states = Arc::clone(states);
                 let state = states[i].clone();
-                let messages = Arc::clone(&messages[&state.id]);
-                sends[i % sends.len()].send(GameObjectWithState(state, states, time, messages)).unwrap();
+                let message: Arc<Vec<Arc<Box<Message>>>>;
+                if let Some(m) = messages.get(&state.id) {
+                    message = m.clone();
+                }
+                else {
+                    message = Arc::new(vec![]);
+                }
+                sends[i % sends.len()].send(GameObjectWithState(state, states, time, message)).unwrap();
             }
             let mut next_states: Vec<GameObjectWithID> = vec![];
             let mut next_messages: HashMap<u64, Arc<Vec<Arc<Box<Message>>>>> = HashMap::new();
@@ -201,16 +218,16 @@ pub mod fungine {
                         Entry::Occupied(mut entry) => {
                             let entry = entry.get_mut();
                             if let Some(m) = Arc::get_mut(entry) {
-                                m.push(Arc::clone(&message.message));
+                                m.push(message.message.clone());
                             }
                         },
                         Entry::Vacant(entry) => {
-                            entry.insert(Arc::new(vec![message.message]));
-                        },
+                            entry.insert(Arc::new(vec![message.message.clone()]));
+                        }
                     }
                 }
             }
-            Arc::new(next_states)
+            (Arc::new(next_states), Arc::new(next_messages))
         }
     }
 }
@@ -218,13 +235,13 @@ pub mod fungine {
 #[cfg(test)]
 mod tests {
     use std::sync::Arc;
-    use fungine::{ Fungine, GameObject, Message, GameObjectWithID };
-    use stopwatch::{Stopwatch};
+    use fungine::{ Fungine, GameObject, Message, GameObjectWithID, UpdateResult, MessageWithID };
+    use stopwatch::{ Stopwatch };
 
     // A GameObject implementation with some state
     #[derive(Clone, Debug)]
     struct TestGameObject {
-        value: i32,
+        pub value: i32,
     }
 
     impl GameObject for TestGameObject {
@@ -233,14 +250,70 @@ mod tests {
         }
 
         // The update function simply increments the internal counter
-        fn update(&self, _current_state: Arc<Vec<GameObjectWithID>>, _messages: Vec<Box<Message>>, _frame_time: f32) -> Box<GameObject> {
-            Box::new(TestGameObject {
-                value: self.value + 1
-            })
+        fn update(&self, _current_state: Arc<Vec<GameObjectWithID>>, _messages: Arc<Vec<Arc<Box<Message>>>>, _frame_time: f32) -> UpdateResult {
+            UpdateResult {
+                state: Box::new(TestGameObject {
+                    value: self.value + 1
+                }),
+                messages: vec![]
+            }
         }
     }
     unsafe impl Send for TestGameObject {}
     unsafe impl Sync for TestGameObject {}
+
+    #[derive(Clone, Debug)]
+    struct TestMessage {
+        pub value: i32
+    }
+
+    impl Message for TestMessage {
+        fn box_clone(&self) -> Box<Message> {
+            Box::new((*self).clone())
+        }
+    }
+    unsafe impl Send for TestMessage {}
+    unsafe impl Sync for TestMessage {}
+
+
+    #[derive(Clone, Debug)]
+    struct MessageGameObject {
+        pub value: i32
+    }
+
+    impl GameObject for MessageGameObject {
+        fn box_clone(&self) -> Box<GameObject> {
+            Box::new((*self).clone())
+        }
+
+        // The update function simply increments the internal counter
+        fn update(&self, current_state: Arc<Vec<GameObjectWithID>>, messages: Arc<Vec<Arc<Box<Message>>>>, _frame_time: f32) -> UpdateResult {
+            let mut new_value: i32 = self.value;
+            for message in messages.clone().iter() {
+                let message: Box<Message> = message.box_clone();
+                if let Some(message) = message.downcast_ref::<TestMessage>() {
+                    new_value += message.value;
+                }
+            }
+            let mut new_messages = vec![];
+            for state in current_state.iter() {
+                new_messages.push(Arc::new(Box::new(MessageWithID {
+                    id: state.id,
+                    message: Arc::new(Box::new(TestMessage {
+                        value: 1
+                    }))
+                })));
+            }
+            UpdateResult {
+                state: Box::new(MessageGameObject {
+                    value: new_value
+                }),
+                messages: new_messages
+            }
+        }
+    }
+    unsafe impl Send for MessageGameObject {}
+    unsafe impl Sync for MessageGameObject {}
 
 
     // A single object, single iteration test to make sure state is passed around correctly
@@ -293,6 +366,37 @@ mod tests {
             let final_state: Box<GameObject> = (final_state.game_object).box_clone();
             if let Some(object) = final_state.downcast_ref::<TestGameObject>() {
                 assert_eq!(1000, object.value);
+            }
+            else {
+                assert!(false);
+            }
+        }
+    }
+
+    // Test messages can be sent, read and processed
+    #[test]
+    fn message_test() {
+        let mut initial_state = vec![];
+        for i in 0u64..2u64 {
+            let initial_object = MessageGameObject {
+                value: 0
+            };
+            let initial_object = Box::new(initial_object) as Box<GameObject>;
+            let initial_object = Arc::new(initial_object);
+            let initial_object = GameObjectWithID {
+                id: i,
+                game_object: initial_object
+            };
+            initial_state.push(initial_object);
+        }
+        let initial_state = Arc::new(initial_state);
+        let engine = Fungine::new(&initial_state);
+        let next_states = engine.run_steps(2, 1f32);
+        for i in 0..2 {
+            let next_state = next_states[i].clone();
+            let next_state: Box<GameObject> = (next_state.game_object).box_clone();
+            if let Some(next_object) = next_state.downcast_ref::<MessageGameObject>() {
+                assert_eq!(2, next_object.value);
             }
             else {
                 assert!(false);
